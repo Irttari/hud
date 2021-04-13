b83c78537064ce01f99b76e91e2248afde9c0901
Irttari <82382295+Irttari@users.noreply.github.com>
2021-04-13 21:18:52 +0300

    Add files via upload
    Siitä "broidi" hudia :)

---
+++ hud/__resource.lua
@@ -0,0 +1,13 @@
+
+ui_page 'html/ui.html'
+
+client_script 'client.lua'
+
+server_script 'server.lua'
+
+files {
+	'html/ui.html',
+	'html/main.js',
+	'html/style.css',
+}
+
---
+++ hud/server.lua
@@ -0,0 +1,56 @@
+ESX = nil
+local RegisteredSocieties = {}
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+TriggerEvent('es:addCommand', 'firmantili', function(source)
+	local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+	
+	if xPlayer.job.grade_name == 'boss' then
+		local society = GetSociety(xPlayer.job.name)
+
+		if society ~= nil then
+			TriggerEvent('esx_addonaccount:getSharedAccount', society.account, function(account)
+				money = account.money
+			end)
+		else
+			money = 0
+		end
+		
+		--[[TriggerClientEvent("pNotify:SetQueueMax", source, "lmao", 5)
+		TriggerClientEvent("pNotify:SendNotification", source, {
+			text = "Firmasi tilillä on ".. money .."€!",
+			type = "success",
+			progressBar = false,
+			queue = "lmao",
+			timeout = 4000,
+			layout = "centerRight"
+		})]]--
+                TriggerClientEvent('esx:showNotification', source, 'Firmasi tilillä on ~g~'.. money ..'€')
+	else
+		--[[TriggerClientEvent("pNotify:SetQueueMax", source, "lmao", 5)
+		TriggerClientEvent("pNotify:SendNotification", source, {
+			text = "Oletko edes firman johdossa?",
+			type = "success",
+			progressBar = false,
+			queue = "lmao",
+			timeout = 4000,
+			layout = "centerRight" 
+		})]]--
+               TriggerClientEvent('esx:showNotification', source, 'Oletko edes firman johdossa?')
+
+	end
+end, {help = "firman tilin"})
+
+TriggerEvent('esx_society:getSocieties', function(societies) 
+	RegisteredSocieties = societies
+end)
+
+function GetSociety(name)
+  for i=1, #RegisteredSocieties, 1 do
+    if RegisteredSocieties[i].name == name then
+      return RegisteredSocieties[i]
+    end
+  end
+end
